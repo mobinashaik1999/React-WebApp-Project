890903868cef1b054563d987ba62e7b8cdd8caf0
mobinaShaik <mobinaShaik1999@gmail.com>
2025-12-03 20:40:17 +0400

    Replaced Cors logic with new one

--- server.js
+++ server.js
@@ -3,14 +3,30 @@ const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+// app.use(cors());
 // ✅ Allow both local dev and GitHub Pages frontend
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://mobinashaik1999.github.io"
+];
+
 app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://mobinashaik1999.github.io"
-  ]
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
 }));
+
+// app.use(cors({
+//   origin: [
+//     "http://localhost:3000",
+//     "https://mobinashaik1999.github.io"
+//   ]
+// }));
 
 app.use(express.json());
 
